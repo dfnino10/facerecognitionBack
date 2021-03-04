19e8c6ea915d7fb2f1f29ecfaa8a1578d61e5e53
dfnino10 <df.nino10@uniandes.edu.co>
2021-03-04 16:06:32 -0500

    More on connecting to the heroku database

--- server.js
+++ server.js
@@ -11,10 +11,8 @@ const image = require('./controllers/image');
 const db = knex({
   client: 'pg',
   connection: {
-    host: 'postgresql-clean-37566',
-    user: 'postgres',
-    password: 'postgres',
-    database: 'smart-brain',
+    host: process.env.DATABASE_URL,
+    ssl: true,
   },
 });
 
